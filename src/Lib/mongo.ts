4003c72fe3b2ce3e36aa80b063cd973938c2edcb
webdesirewebsolution <webdeveloperji71@gmail.com>
2024-11-27 12:14:18 +0530

    changes in mongo lib

--- src/Lib/mongo.ts
+++ src/Lib/mongo.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGOURL as string);
+const client = new MongoClient('mongodb+srv://d21350180:Deepakd213@cluster0.eqn7ymc.mongodb.net/?retryWrites=true&w=majority');
 async function start() {
     await client.connect();
     console.log("Connected");

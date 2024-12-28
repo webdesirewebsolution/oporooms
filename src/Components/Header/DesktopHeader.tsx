3311aeeb283e81ef9fe5265f5346e3ffdb9f8f09
webdesirewebsolution <webdeveloperji71@gmail.com>
2024-12-28 15:17:25 +0530

    fix: update corporate trust count and improve earning time display; refactor newDate function for clarity

--- src/Components/Header/DesktopHeader.tsx
+++ src/Components/Header/DesktopHeader.tsx
@@ -11,7 +11,7 @@ const DesktopHeader = () => {
                     <MdOutlineBusinessCenter size={30}/>
                 <div className='flex flex-col items-center'>
                     <p className='text-[#212121] font-medium'>OPO for Business</p>
-                    <p className='text-[rgba(0,0,0,0.54)] text-lg'>Trusted by 2000 Corporates</p>
+                    <p className='text-[rgba(0,0,0,0.54)] text-lg'>Trusted by 200 Corporates</p>
                 </div>
             </Link>
 
@@ -19,7 +19,7 @@ const DesktopHeader = () => {
                     <FaBuilding size={23}/>
                 <div className='flex flex-col items-center'>
                     <p className='text-[#212121] font-medium'>List Your Property</p>
-                    <p className='text-[rgba(0,0,0,0.54)] text-lg'>Start earning on 30 mins</p>
+                    <p className='text-[rgba(0,0,0,0.54)] text-lg'>Start earning on 10 mins</p>
                 </div>
             </Link>
 

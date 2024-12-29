34297724982e1e6d949889f2d4a9e231a6305f2f
webdesirewebsolution <webdeveloperji71@gmail.com>
2024-12-29 06:52:12 +0530

    feat: enhance Next.js configuration with bundle analyzer and ignore ESLint/type errors during builds

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  productionBrowserSourceMaps: false,
+  experimental: {
+    optimizePackageImports: ['@mui/x-data-grid', 'jodit-react'],
+    serverSourceMaps: false
+  },
   images: {
     remotePatterns: [
       {
@@ -27,4 +44,8 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer(nextConfig)
